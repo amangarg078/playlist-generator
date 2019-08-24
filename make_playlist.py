649ba990ed4dc4ef0e346423411fbb6b2579623b
Aman Garg <amangarg078@gmail.com>
2019-08-24 12:48:26 +0530

    Add files via upload

---
+++ make_playlist.py
@@ -0,0 +1,44 @@
+from __future__ import unicode_literals
+
+import argparse
+import os
+import sys
+
+class PlaylistCreator(object):
+    def __init__(self, directory):
+        self.directory = directory
+
+    def create_playlist(self, playlist_path, files):
+        with open(playlist_path, 'w+') as p:
+            for f in files:
+                print >> p, f
+
+    def run(self):
+        for (root, dirs, filenames) in os.walk(self.directory, topdown=False):
+            media_files = []
+            for _file in filenames:
+                extension = os.path.splitext(_file)[1]
+                if extension in ['.mp4', '.mp3', '.mkv', '.aac']:
+                    media_files.append(_file)
+
+            # no file to add to playlist
+            if not media_files:
+                continue
+            playlist_filename = os.path.split(root)[-1]
+            playlist_path = os.path.join(root, "Playlist - " + playlist_filename + ".m3u")
+            self.create_playlist(playlist_path, media_files)
+
+if __name__ == '__main__':
+    # take the target root directory as argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", help="Select the root directory",
+                    type=str, nargs='?', default=os.getcwd())
+    args = parser.parse_args()
+    directory = args.directory
+
+    # if provided directory is not a directory, stop execution
+    if not os.path.isdir(directory):
+        print "Please enter a valid directory or place the file in the root directory and don't provide any arguments to run the script."
+        sys.exit()
+    playlist_creator = PlaylistCreator(directory)
+    playlist_creator.run()
