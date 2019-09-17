ebb02f35c91d5500d7e65f3fc00dc96c2073ef02
Aman Garg <amangarg078@gmail.com>
2019-09-17 18:56:00 +0530

    support alpha numeric sorting

--- make_playlist.py
+++ make_playlist.py
@@ -2,7 +2,18 @@ from __future__ import unicode_literals
 
 import argparse
 import os
+import re
 import sys
+
+def get_sort_key(s):
+    """
+    Return key for alpha numeric sorting
+    """
+    match = re.findall(r"^(\d+)", s)
+    if match:
+        return int(match[0])
+    else:
+        return s
 
 class PlaylistCreator(object):
     def __init__(self, directory):
@@ -26,7 +37,7 @@ class PlaylistCreator(object):
                 continue
             playlist_filename = os.path.split(root)[-1]
             playlist_path = os.path.join(root, "Playlist - " + playlist_filename + ".m3u")
-            media_files.sort()
+            media_files = sorted(media_files, key=get_sort_key)
             self.create_playlist(playlist_path, media_files)
 
 if __name__ == '__main__':
